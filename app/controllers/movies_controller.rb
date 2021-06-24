44167f2d108c3cf5b57cbaf42a735e7eeed79e5f
Joanna Klosinska <joanna.klosinska@smartpension.co.uk>
2021-06-24 18:17:42 +0200

    Create movies controller

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class MoviesController < ApplicationController
+  def show
+    title = params[:title]
+    movie = Movie.find_by(title: title)
+    if movie
+      render json: movie
+    else
+      CreateMovieWorker.perform_async(title)
+      render json: 'not_found'
+    end
+  end
+
+  def index
+    movies = Movie.all
+    render json: movies
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
+  resources :movies, only: %i[show index], param: :title
 end
