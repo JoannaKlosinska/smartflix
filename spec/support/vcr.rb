a9cc19447f3e16da77c3965a12f04556ddbe3be2
Joanna Klosinska <joanna.klosinska@smartpension.co.uk>
2021-10-29 11:02:30 +0200

    Modify default_cassette_options

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,6 +4,6 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.ignore_localhost = true
-  config.default_cassette_options = { record: :new_episodes }
+  config.default_cassette_options = { record: :once, match_requests_on: %i[body method] }
   config.filter_sensitive_data('apikey') { Rails.application.credentials.omdb[:api_key] }
 end
