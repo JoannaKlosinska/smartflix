3305d2c48821c300b8e5030f205171bcae47c18e
Joanna Klosinska <joanna.klosinska@smartpension.co.uk>
2022-08-25 08:55:47 +0200

    Improve create_movie_worker_spec

--- spec/workers/create_movie_worker_spec.rb
+++ spec/workers/create_movie_worker_spec.rb
@@ -7,13 +7,12 @@ RSpec.describe CreateMovieWorker, type: :worker do
     subject { described_class.new.perform(title) }
 
     let(:title) { 'Batman' }
+    let(:import_movie_service) { instance_double(ImportMovie) }
 
-    before do
-      allow(ImportMovie).to receive(:new).with(title)
-    end
+    it 'calls ImportMovie service' do
+      expect(ImportMovie).to receive(:new).with(title).and_return(import_movie_service)
+      expect(import_movie_service).to receive(:call)
 
-    it 'runs import movie service' do
-      expect(ImportMovie.new(title)).to receive(:call)
       subject
     end
   end
